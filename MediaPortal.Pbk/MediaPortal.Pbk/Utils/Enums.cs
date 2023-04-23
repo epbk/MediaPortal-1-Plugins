bb4a06254d6623e61ef3ce851e36b57d8fa2ffbb
epbk <epbk@epbk.cz>
2023-04-23 22:52:59 +0200

    Library MediaPortal.Pbk: Added Enums class

---
+++ MediaPortal.Pbk/MediaPortal.Pbk/Utils/Enums.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace MediaPortal.Pbk.Utils
+{
+    public static class Enums
+    {
+        public static string[] GetEnumNames(Type tEnum)
+        {
+            FieldInfo[] fields = tEnum.GetFields();
+            string[] result = tEnum.GetEnumNames();
+
+            for (int iN = 0; iN < result.Length; iN++)
+            {
+                string strName = result[iN];
+
+                for (int iF = 0; iF < fields.Length; iF++)
+                {
+                    FieldInfo fi = fields[iF];
+
+                    if (fi.Name.Equals(strName))
+                    {
+                        DescriptionAttribute attr = (DescriptionAttribute)Attribute.GetCustomAttribute(fi, typeof(DescriptionAttribute));
+
+                        if (attr != null)
+                        {
+                            result[iN] = attr.Description;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
