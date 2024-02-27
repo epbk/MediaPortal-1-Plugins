568f26de255ca87c2a9f3e419f11386a2019cf2b
epbk <epbk@epbk.cz>
2024-02-27 19:04:46 +0100

    Library MediaPortal.Pbk: StringBuilderExtensions : Add new method GetUTF8Bytes

--- MediaPortal.Pbk/MediaPortal.Pbk/Extensions/StringBuilderExtensions.cs
+++ MediaPortal.Pbk/MediaPortal.Pbk/Extensions/StringBuilderExtensions.cs
@@ -80,6 +80,54 @@ namespace MediaPortal.Pbk.Extensions
             return self;
         }
 
+        /// <summary>
+        /// Encodes current <see cref="System.Text.StringBuilder" /> to UTF8 and stores the result in the data argument from position given by iSize.
+        /// </summary>
+        /// <param name="self">The current <see cref="System.Text.StringBuilder" /> instance.</param>
+        /// <param name="data">The byte array for the UTF8 destination bytes.</param>
+        /// <param name="iSize">Cuurent position of data argument. Advanced after conversion.</param>
+        /// <returns>The current <see cref="System.Text.StringBuilder" />.</returns>
+        public static StringBuilder GetUTF8Bytes(this StringBuilder self, byte[] data, ref int iSize)
+        {
+            for (int iIdxSb = 0; iIdxSb < self.Length; iIdxSb++)
+            {
+                if (iSize >= data.Length)
+                    throw new IndexOutOfRangeException("GetUTF8Bytes(): data buffer out of range");
+
+                int iValue = self[iIdxSb];
+
+                //surrogate code check
+                if (iValue >= 0xd800)
+                    iValue = 0x10000 + (((iValue - 0xd800) << 10) | (self[++iIdxSb] - 0xdc00));
+
+                if (iValue <= 0x7F)
+                    data[iSize++] = (byte)iValue;
+                else
+                {
+                    if (iValue <= 0x07FF)
+                        data[iSize++] = (byte)((iValue >> 6) | 0xC0);
+                    else
+                    {
+                        if (iValue <= 0xFFFF)
+                            data[iSize++] = (byte)((iValue >> 12) | 0xE0);
+                        else if (iValue <= 0x10FFFF)
+                        {
+                            data[iSize++] = (byte)((iValue >> 18) | 0xF0);
+                            data[iSize++] = (byte)((iValue >> 12) & 0x3F | 0x80);
+                        }
+                        else
+                            throw new OverflowException("GetUTF8Bytes(): char is too big");
+
+                        data[iSize++] = (byte)((iValue >> 6) & 0x3F | 0x80);
+                    }
+
+                    data[iSize++] = (byte)((iValue & 0x3F) | 0x80);
+                }
+            }
+
+            return self;
+        }
+
         private static bool isWhiteSpace(char c)
         {
             for (int i = 0; i < _WhiteSpaceChars.Length; i++)
