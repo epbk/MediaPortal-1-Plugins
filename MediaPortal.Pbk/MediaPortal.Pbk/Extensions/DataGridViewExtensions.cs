fd58ab4fac7bf518ebbf3363f612aad0ceb5af37
epbk <epbk@epbk.cz>
2024-06-07 14:01:52 +0200

    Library MediaPortal.Pbk: Extensions update

---
+++ MediaPortal.Pbk/MediaPortal.Pbk/Extensions/DataGridViewExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MediaPortal.Pbk.Extensions
+{
+    /// <summary>
+    /// DataGridView extensions
+    /// </summary>
+    public static class DataGridViewExtensions
+    {
+        /// <summary>
+        /// Performs the specified action on each <see cref="System.Windows.Forms.DataGridViewRow" /> of the <see cref="System.Windows.Forms.DataGridViewSelectedRowCollection" />.
+        /// </summary>
+        /// <param name="self"><see cref="System.Windows.Forms.DataGridViewSelectedRowCollection" /> instance.</param>
+        /// <param name="action">The <see cref="T:System.Action`1" /> delegate to perform on each <see cref="System.Windows.Forms.DataGridViewRow" /> of the <see cref="System.Windows.Forms.DataGridViewSelectedRowCollection" />.</param>
+        /// <exception><paramref name="self" /> is null.</exception>
+        /// <exception><paramref name="action" /> is null.</exception>
+        public static void ForEach(this DataGridViewSelectedRowCollection self, Action<DataGridViewRow> action)
+        {
+            if (self == null || action == null)
+                throw new ArgumentNullException();
+
+            for (int i = 0; i < self.Count; i++)
+                action(self[i]);
+        }
+    }
+}
