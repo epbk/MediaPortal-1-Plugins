fca8701d18c16c8f28223dc661f621a7b451a7e0
epbk <epbk@epbk.cz>
2025-01-11 16:08:32 +0100

    Plugin IptvChannels: Added MPE Installer v0.9.3.0

--- IptvChannels/IptvChannels/Properties/AssemblyInfo.cs
+++ IptvChannels/IptvChannels/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using MediaPortal.Common.Utils;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.2.0")]
-[assembly: AssemblyFileVersion("0.9.2.0")]
+[assembly: AssemblyVersion("0.9.3.0")]
+[assembly: AssemblyFileVersion("0.9.3.0")]
 
 [assembly: CompatibleVersion("1.11.0.0", " 1.11.0.0")]
 [assembly: UsesSubsystem("TVE")]
